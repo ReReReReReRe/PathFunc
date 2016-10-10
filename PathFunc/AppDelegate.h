408ff8d4e54c532b96e5afeb1be7d1dbbd094e83
李财 <45634626@qq.com>
2016-10-10 14:18:45 +0800

    Initial Commit

---
+++ PathFunc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PathFunc
+//
+//  Created by 李财 on 16/10/10.
+//  Copyright © 2016年 李财. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PathFunc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PathFunc
+//
+//  Created by 李财 on 16/10/10.
+//  Copyright © 2016年 李财. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
